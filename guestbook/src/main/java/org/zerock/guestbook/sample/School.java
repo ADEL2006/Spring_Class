fd4e191c6e4628d2c0a3e79f52f70f6a8cabb26c
양원준 <anyas1398@gmail.com>
2023-08-30 14:05:45 +0900

    Create School.java

---
+++ guestbook/src/main/java/org/zerock/guestbook/sample/School.java
@@ -0,0 +1,9 @@
+package org.zerock.guestbook.sample;
+
+public class School {
+    public School() {
+        System.out.println("================");
+        System.out.println("School 생성자 호출");
+        System.out.println("================");
+    }
+}
