002541d17521dea4e3aabdac4016f583d7fc7466
양원준 <anyas1398@gmail.com>
2023-08-30 14:03:11 +0900

    .

--- guestbook/src/main/java/org/zerock/guestbook/GuestbookApplication.java
+++ guestbook/src/main/java/org/zerock/guestbook/GuestbookApplication.java
@@ -2,11 +2,14 @@ package org.zerock.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GuestbookApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(GuestbookApplication.class, args);
+
+		ConfigurableApplicationContext applicationContext =
+			SpringApplication.run(GuestbookApplication.class, args);
 	}
 
 }
