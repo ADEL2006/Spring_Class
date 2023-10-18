b5bdc6f5773b865a6cbc8e8e32e2b5f0a9e37a9b
양원준 <anyas1398@gmail.com>
2023-10-18 12:29:10 +0900

    .

--- guestbook/src/test/java/org/zerock/guestbook/repository/MemoRepositoryTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/repository/MemoRepositoryTests.java
@@ -3,10 +3,17 @@ package org.zerock.guestbook.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.MemoEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemoRepositoryTests {
@@ -17,9 +24,9 @@ class MemoRepositoryTests {
     void testInsert() {
         IntStream
                 .rangeClosed(1, 100)
-                .forEach( i -> {
+                .forEach(i -> {
                     MemoEntity memoEntity = MemoEntity.builder()
-                            .memoText("sample...." + i)
+                            .memoText("Sample...." + i)
                             .build();
                     memoRepository.save(memoEntity);
                 });
@@ -30,14 +37,17 @@ class MemoRepositoryTests {
         Long mno = 100L;
         Optional<MemoEntity> result = memoRepository.findById(mno);
         if (result.isPresent()) {
-            result.get();
-            System.out.println(memoRepository);
+            MemoEntity memoEntity = result.get();
+            System.out.println(memoEntity);
+        } else {
+            System.out.println("empty!!");
         }
     }
 
     @Test
     void testUpdate() {
-        MemoEntity memoEntity = MemoEntity.builder()
+        MemoEntity memoEntity
+                = MemoEntity.builder()
                 .mno(100L)
                 .memoText("Update......")
                 .build();
@@ -47,9 +57,67 @@ class MemoRepositoryTests {
     @Test
     void testDelete() {
         Long mno = 100L;
+//        memoRepository.deleteById(mno);
         MemoEntity memoEntity = MemoEntity.builder()
-                .mno(100L)
+                .mno(99L)
                 .build();
         memoRepository.delete(memoEntity);
+    }
+
+
+    @Test
+    public void testPageDefault() {
+        Pageable pageable
+                = PageRequest.of(0, 20);
+        Page<MemoEntity> result
+                = memoRepository.findAll(pageable);
+        System.out.println(result);
+
+        System.out.println("---------------------------------------");
+
+        System.out.println("Total Pages: " + result.getTotalPages()); // 전체 페이지 번호
+        System.out.println("Total Count: " + result.getTotalElements()); // 전체 데이터 수
+        System.out.println("Page Number: " + result.getNumber()); // 현재 페이지 번호, 0부터 시작
+
+        System.out.println("Page Size: " + result.getSize()); // 페이지 크기
+
+        System.out.println("has next page: " + result.hasNext()); // 다음 페이지 존재 여부
+
+        System.out.println("first page: " + result.isFirst()); // 첫 페이지 여부
+
+        result.get().forEach(memoEntity -> {
+            System.out.println(memoEntity);
+        });
+
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort = Sort.by("mno").descending();
+
+        Pageable pageable
+                = PageRequest.of(0, 10, sort);
+
+        Page<MemoEntity> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> System.out.println(memo));
+    }
+
+    @Test
+    public void testQueryMethod() {
+     List<MemoEntity> result = memoRepository.findByMnoBetweenOrderByMnoDesc(10L, 30L);
+     result.forEach(memoEntity -> {
+         System.out.println(memoEntity);
+     });
+    }
+
+    @Test
+    void testQueryMethod2() {
+        Long mno = 50L;
+        Optional<MemoEntity> result = memoRepository.findByMno(mno);
+        if(result.isPresent()) {
+            System.out.println(result.get());
+        }
+
     }
 }
