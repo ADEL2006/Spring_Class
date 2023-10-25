7f6683fcf7a2098a9ae1de6214e2e4b32f773bf5
양원준 <anyas1398@gmail.com>
2023-10-25 15:27:48 +0900

    .

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServicelmpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServicelmpl.java
@@ -1,15 +1,22 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.GuestbookEntity;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class GuestbookServicelmpl implements GuestbookService{
+    private GuestbookRepository guestbookRepository;
 
     @Override
     public Long register(GuestbookDTO dto) {
-        return null;
+        log.info("DTO: {}", dto);
+        GuestbookEntity guestbookEntity = dtoToEntity(dto);
+        return guestbookEntity.getGno();
     }
 }
