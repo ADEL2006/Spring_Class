413ce4ad24f11a5704cfac4b3216ac4eb122947f
양원준 <anyas1398@gmail.com>
2023-10-30 14:32:27 +0900

    .

---
+++ mid/src/test/java/com/dgswhs/mid/MidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgswhs.mid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
