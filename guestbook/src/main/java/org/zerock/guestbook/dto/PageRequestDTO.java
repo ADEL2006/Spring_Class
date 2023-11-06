7a0ec854544de4b2fa4a96fe36da03b5809281c8
양원준 <anyas1398@gmail.com>
2023-11-06 15:04:59 +0900

    .

--- guestbook/src/main/java/org/zerock/guestbook/dto/PageRequestDTO.java
+++ guestbook/src/main/java/org/zerock/guestbook/dto/PageRequestDTO.java
@@ -1,9 +1,26 @@
 package org.zerock.guestbook.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@Setter
+@ToString
 public class PageRequestDTO {
     private int page;
     private int size;
 
-    
-    
+    public PageRequestDTO() {
+        page = 1;
+        size = 10;
+    }
+
+    public PageRequest getPageable(Sort sort) {
+        return PageRequest.of(page-1, size, sort);
+    }
 
 }
---
+++ guestbook/src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
@@ -0,0 +1,29 @@
+package org.zerock.guestbook.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Data
+public class PageResultDTO<DTO, EN> {
+    private List<DTO> dtoList;
+    private int totalPage;
+    private int page;
+    private int size;
+    private int start, end;
+    private boolean prev, next;
+    private List<Integer> pageList;
+
+    public PageResultDTO(
+            Page<EN> result,
+            Function<EN, DTO> fn
+    ) {
+        dtoList = result.stream().map(fn).collect(Collectors.toList());
+        totalPage = result.getTotalPages();
+
+    }
+
+}
