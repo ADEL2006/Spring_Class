42434fbced171104062a3923cf076ff2e4c8932f
양원준 <anyas1398@gmail.com>
2023-11-13 15:48:34 +0900

    .

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -2,8 +2,11 @@ package org.zerock.guestbook.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
 @RestController
@@ -12,4 +15,9 @@ import org.zerock.guestbook.service.GuestbookService;
 @Log4j
 public class GuestbookController {
     private final GuestbookService guestbookService;
+
+    @GetMapping("{gno}")
+    public GuestbookDTO read(@PathVariable("gno") Long gno){
+        return guestbookService.read(gno);
+    }
 }
