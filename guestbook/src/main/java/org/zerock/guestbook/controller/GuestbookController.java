26d87d81c1236b3e3f309df98517d21ed6307777
양원준 <anyas1398@gmail.com>
2023-11-13 15:57:21 +0900

    .

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -2,11 +2,14 @@ package org.zerock.guestbook.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
 @RestController
@@ -16,8 +19,14 @@ import org.zerock.guestbook.service.GuestbookService;
 public class GuestbookController {
     private final GuestbookService guestbookService;
 
-    @GetMapping("{gno}")
+    @GetMapping("/{gno}")
     public GuestbookDTO read(@PathVariable("gno") Long gno){
         return guestbookService.read(gno);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<PageResultDTO> getList(PageRequestDTO pageRequestDTO) {
+        return ResponseEntity.ok(guestbookService.getList(pageRequestDTO));
+
     }
 }
