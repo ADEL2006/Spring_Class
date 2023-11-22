c88f81ae7c1e42b3738fa20db8bbbb5d181dd4e3
양원준 <anyas1398@gmail.com>
2023-11-22 13:52:49 +0900

    Update MemberController.java

--- guestbook/src/main/java/org/zerock/guestbook/controller/MemberController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/MemberController.java
@@ -22,13 +22,33 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    // C
+    // localhost:8080/member/
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public String register(@RequestBody MemberDTO dto) {
         memberService.register(dto);
-        return "";
+        return "CREATE OK";
     }
 
+    // R
+    // localhost:8080/member/1
     @GetMapping("/{mno}")
-    
+    public MemberDTO read(@PathVariable("mno") Long mno) {
+        MemberDTO memberDTO = memberService.read(mno);
+        return memberDTO;
+    }
+
+    // U
+    @PutMapping("/")
+    public String modify(@RequestBody MemberDTO dto) {
+        memberService.modify(dto);
+        return "UPDATE OK";
+    }
+
+    // D
+    @DeleteMapping("/{mno}")
+    public void remove(@PathVariable("mno") Long mno) {
+        memberService.remove(mno);
+    }
 }
